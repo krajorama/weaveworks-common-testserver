86a3a419d2f844362b52a855125799c105b512cc
György Krajcsovits <gyorgy.krajcsovits@grafana.com>
2022-12-01 09:24:15 +0100

    Add server handlers for each test case
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- go.mod
+++ go.mod
@@ -3,20 +3,40 @@ module github.com/krajorama/weaveworks-common-testserver
 go 1.19
 
 require (
+	github.com/opentracing/opentracing-go v1.1.0
+	github.com/uber/jaeger-client-go v2.28.0+incompatible
 	github.com/weaveworks/common v0.0.0-20221125125352-88dcecd133b1
 	google.golang.org/grpc v1.31.0
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/gogo/googleapis v1.1.0 // indirect
 	github.com/gogo/protobuf v1.3.0 // indirect
 	github.com/gogo/status v1.0.3 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/opentracing-contrib/go-stdlib v0.0.0-20190519235532-cf7a6c988dc9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.12.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/sirupsen/logrus v1.6.0 // indirect
+	github.com/uber/jaeger-lib v2.2.0+incompatible // indirect
+	github.com/weaveworks/promrus v1.2.0 // indirect
+	go.uber.org/atomic v1.5.1 // indirect
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.0.0-20200825202427-b303f430e36d // indirect
 	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 	"net/http"
 
 	"github.com/krajorama/weaveworks-common-testserver/server"
+	opentracing "github.com/opentracing/opentracing-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
 	"github.com/weaveworks/common/httpgrpc"
+	"github.com/weaveworks/common/middleware"
+	"github.com/weaveworks/common/user"
 	"google.golang.org/grpc"
 )
 
@@ -39,14 +43,47 @@ func main() {
 
 	done := make(chan struct{})
 
-	server, err := newTestServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	jaeger := jaegercfg.Configuration{}
+	closer, err := jaeger.InitGlobalTracer("test")
+	if err != nil {
+		panic("Could not init tracing")
+	}
+	defer closer.Close()
+
+	server, err := newTestServer(middleware.Tracer{}.Wrap(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodDelete:
 			done <- struct{}{}
+			return
 		default:
 		}
-		fmt.Fprint(w, "world")
-	}))
+		switch r.URL.Path {
+		case "/hello":
+			fmt.Fprint(w, "world")
+			return
+		case "/error500":
+			http.Error(w, "server error message", http.StatusInternalServerError)
+		case "/error400":
+			http.Error(w, "request error message", http.StatusForbidden)
+		case "/trace":
+			// This depends on having the http.HandlerFunc being wrapped in the tracer middleware
+			span := opentracing.SpanFromContext(r.Context())
+			if span != nil {
+				fmt.Fprint(w, span.BaggageItem("name"))
+			} else {
+				http.Error(w, "could not load span from context", http.StatusBadRequest)
+			}
+		case "/orgid":
+			id, _, err := user.ExtractOrgIDFromHTTPRequest(r)
+			if err == nil {
+				fmt.Fprint(w, id)
+			} else {
+				http.Error(w, "could not extract orgid", http.StatusInternalServerError)
+			}
+		default:
+			http.Error(w, "Unrecognized test path", http.StatusBadRequest)
+		}
+	}),))
 
 	if err != nil {
 		panic(fmt.Sprintf("Could not start server %e", err))
